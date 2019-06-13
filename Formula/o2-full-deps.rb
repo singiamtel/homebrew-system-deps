9e5e856909fc222634f1264a5dab6fe0c15b0b8a
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-06-13 14:29:38 +0200

    Initial import

---
+++ Formula/o2-full-deps.rb
@@ -0,0 +1,38 @@
+class O2FullDeps < Formula
+  desc "ALICE O2 System Dependencies via Homebrew"
+  homepage "https://alisw.github.io"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "19.6-1"
+
+  depends_on "apache-arrow"
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "cmake"
+  depends_on "coreutils"
+  depends_on "gettext"
+  depends_on "glfw3"
+  depends_on "gmp"
+  depends_on "gsl"
+  depends_on "hub"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "libpng"
+  depends_on "libtool"
+  depends_on "libxml2"
+  depends_on "m4"
+  depends_on "make"
+  depends_on "modules"
+  depends_on "mpfr"
+  depends_on "msgpack"
+  depends_on "openssl"
+  depends_on "pcre"
+  depends_on "pkg-config"
+  depends_on "readline"
+  depends_on "xz"
+  depends_on "zeromq"
+
+  def install
+    system "true"
+  end
+end
