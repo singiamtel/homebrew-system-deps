2f9c32fef4c2e2d81df283824745bcd0e6656f84
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-12-29 01:35:50 +0100

    Add formula for aliBuild (#3)

---
+++ Formula/alibuild.rb
@@ -0,0 +1,52 @@
+class Alibuild < Formula
+  include Language::Python::Virtualenv
+
+  desc "ALICE Build Tool"
+  homepage "http://alisw.github.io/alibuild"
+  url "https://github.com/alisw/alibuild/archive/1.5.5.tar.gz"
+  sha256 "39f199e907c68ee1dce01cd6949659d560543ee77e1add673a10997c2bf02f5f"
+  version "1.5.5"
+
+  resource "alibuild" do
+    url "https://files.pythonhosted.org/packages/98/92/2a836f2f26374857253217ee9a808085c7eda6ff8b6ad11945747d73f757/alibuild-1.5.5.tar.gz"
+    sha256 "32852d9dd5d8ab210cfd59f4d2917e8ed090784722dd61a3a7982c75b71200ea"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/41/bf/9d214a5af07debc6acf7f3f257265618f1db242a3f8e49a9b516f24523a6/certifi-2019.11.28.tar.gz"
+    sha256 "25b64c7da4cd7479594d035c08c2d809eb4aab3a26e5a990ea98cc450c320f1f"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/ad/13/eb56951b6f7950cadb579ca166e448ba77f9d24efc03edd7e55fa57d04b7/idna-2.8.tar.gz"
+    sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/8d/c9/e5be955a117a1ac548cdd31e37e8fd7b02ce987f9655f5c7563c656d5dcb/PyYAML-5.2.tar.gz"
+    sha256 "c0ee8eca2c582d29c3c2ec6e2c4f703d1b7f1fb10bc72317355a746057e7346c"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/01/62/ddcf76d1d19885e8579acb1b1df26a852b03472c0e46d2b959a714c90608/requests-2.22.0.tar.gz"
+    sha256 "11e007a8a2aa0323f5a921e9e6a2d7e4e67d9877e85773fba9ba6419025cbeb4"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/ad/fc/54d62fa4fc6e675678f9519e677dfc29b8964278d75333cf142892caf015/urllib3-1.25.7.tar.gz"
+    sha256 "f3c5fd51747d450d4dcf6f923c81f78f811aab8205fda64b0aba34a4e48b0745"
+  end
+
+  def install
+    venv = virtualenv_create(libexec)
+    venv.pip_install resources
+    venv.pip_install_and_link buildpath
+    bin.install_symlink "#{libexec}/bin/aliBuild" => "aliBuild"
+    bin.install_symlink "#{libexec}/bin/alienv" => "alienv"
+  end
+end
