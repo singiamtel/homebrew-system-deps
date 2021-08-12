77f5e60c31a05e7d3d51ea0a787d34899241c124
Timo Wilken <timo.wilken@cern.ch>
2021-08-12 20:35:57 +0000

    Update alibuild to v1.8.8 (#21)
    Change the URL to the PyPI-hosted tarball, so `brew update-python-resources` is
    easier.

--- Formula/alibuild.rb
+++ Formula/alibuild.rb
@@ -3,36 +3,31 @@ class Alibuild < Formula
 
   desc "ALICE Build Tool"
   homepage "http://alisw.github.io/alibuild"
-  url "https://github.com/alisw/alibuild/archive/v1.8.6.tar.gz"
-  sha256 "c63cd5655fafb9397ba112940b920d428e1a374379727a56ca8612c8a89e03c1"
-  version "1.8.6"
+  url "https://files.pythonhosted.org/packages/d0/19/a4f32e504343c9e2a5fbabb3f409d46d4931b76f912aa12d7aea844552e6/alibuild-1.8.8.tar.gz"
+  sha256 "714f0e8d56ed13385fb7149ca1ee7ba84825a8b73c428d2bcd96a7d7d214bd6f"
+  version "1.8.8"
   depends_on "s3cmd"
   depends_on "pigz"
   depends_on "modules"
-
-  resource "alibuild" do
-    url "https://files.pythonhosted.org/packages/26/a1/59dbb12498e75a86c663e48a5a3a1c5eb811b0e57ce1816397d8fdfbcbf6/alibuild-1.8.6.tar.gz"
-    sha256 "452b498e66fd411758aacb0624304044c07adea2a5336c8dd8fec8f8db8ed13e"
-  end
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
     sha256 "2bbf76fd432960138b3ef6dda3dde0544f27cbf8546c458e60baf371917ba9ee"
   end
 
-  resource "chardet" do
-    url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
-    sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/4e/2af0238001648ded297fb54ceb425ca26faa15b341b4fac5371d3938666e/charset-normalizer-2.0.4.tar.gz"
+    sha256 "f23667ebe1084be45f6ae0538e4a5a865206544097e4e8bbcacf42cd02a348f3"
   end
 
   resource "distro" do
-    url "https://files.pythonhosted.org/packages/a6/a4/75064c334d8ae433445a20816b788700db1651f21bdb0af33db2aab142fe/distro-1.5.0.tar.gz"
-    sha256 "0e58756ae38fbd8fc3020d54badb8eae17c5b9dcbed388b17bb55b8a5928df92"
+    url "https://files.pythonhosted.org/packages/a5/26/256fa167fe1bf8b97130b4609464be20331af8a3af190fb636a8a7efd7a2/distro-1.6.0.tar.gz"
+    sha256 "83f5e5a09f9c5f68f60173de572930effbcc0287bb84fdc4426cb4168c088424"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/ea/b7/e0e3c1c467636186c39925827be42f16fee389dc404ac29e930e9136be70/idna-2.10.tar.gz"
-    sha256 "b307872f855b18632ce0c21c5e45be78c0ea7ae4c15c828c20788b26921eb3f6"
+    url "https://files.pythonhosted.org/packages/cb/38/4c4d00ddfa48abe616d7e572e02a04273603db446975ab46bbcd36552005/idna-3.2.tar.gz"
+    sha256 "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
   end
 
   resource "PyYAML" do
@@ -41,15 +36,15 @@ class Alibuild < Formula
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/6b/47/c14abc08432ab22dc18b9892252efaf005ab44066de871e72a38d6af464b/requests-2.25.1.tar.gz"
-    sha256 "27973dd4a904a4f13b263a19c866c13b92a39ed1c964655f025f3f8d3d75b804"
+    url "https://files.pythonhosted.org/packages/e7/01/3569e0b535fb2e4a6c384bdbed00c55b9d78b5084e0fb7f4d0bf523d7670/requests-2.26.0.tar.gz"
+    sha256 "b8aa58f8cf793ffd8782d3d8cb19e66ef36f7aba4353eec859e74678b01b07a7"
   end
 
   resource "urllib3" do
     url "https://files.pythonhosted.org/packages/4f/5a/597ef5911cb8919efe4d86206aa8b2658616d676a7088f0825ca08bd7cb8/urllib3-1.26.6.tar.gz"
     sha256 "f57b4c16c62fa2760b7e3d97c35b255512fb6b59a259730f36ba32ce9f8e342f"
   end
-  
+
   def install
     virtualenv_install_with_resources :using => "python@3"
     bin.install_symlink "#{libexec}/bin/aliBuild" => "aliBuild"
