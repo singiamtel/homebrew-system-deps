9b7c2a7dce7162a3a4e9eff1542809df3ea33e93
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-08-18 15:33:08 +0200

    Update alibuild.rb

--- Formula/alibuild.rb
+++ Formula/alibuild.rb
@@ -3,12 +3,28 @@ class Alibuild < Formula
 
   desc "ALICE Build Tool"
   homepage "http://alisw.github.io/alibuild"
-  url "https://files.pythonhosted.org/packages/d0/19/a4f32e504343c9e2a5fbabb3f409d46d4931b76f912aa12d7aea844552e6/alibuild-1.8.8.tar.gz"
-  sha256 "714f0e8d56ed13385fb7149ca1ee7ba84825a8b73c428d2bcd96a7d7d214bd6f"
-  version "1.8.8"
+  url "https://files.pythonhosted.org/packages/48/2a/cd497ed06793deced9f7da3637cc3b29226e35d914888a89f7e5fb4c0126/alibuild-1.8.9.tar.gz"
+  sha256 "86b92c29a50ab0e5b19a80d1ffa2161ae8863c2b7b890ddb9495839a34761411"
+  version "1.8.9"
   depends_on "s3cmd"
   depends_on "pigz"
   depends_on "modules"
+  depends_on "libyaml"
+
+  resource "alibuild" do
+    url "https://files.pythonhosted.org/packages/48/2a/cd497ed06793deced9f7da3637cc3b29226e35d914888a89f7e5fb4c0126/alibuild-1.8.9.tar.gz"
+    sha256 "0ec7eba12ead0c3afc8d1dea84e46e2b56e69aea6282aa15afbfa93f11787fc0"
+  end
+
+  resource "boto3" do
+    url "https://files.pythonhosted.org/packages/5f/4d/c86763405b1a53d98257a136c7bb42aa60f5c7e242e66197c43b6616840a/boto3-1.18.23.tar.gz"
+    sha256 "31cc69e665f773390c4c17ce340d2420e45fbac51d46d945cc4a58d483ec5da6"
+  end
+
+  resource "botocore" do
+    url "https://files.pythonhosted.org/packages/a6/74/c4f3118df00ef7b7d975571ed2f3cbac71e87b396b2171be9879d7b57ffb/botocore-1.21.23.tar.gz"
+    sha256 "d0146d31dbc475942b578b47dd5bcf94d18fbce8c6d2ce5f12195e005de9b754"
+  end
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
@@ -30,6 +46,16 @@ class Alibuild < Formula
     sha256 "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
   end
 
+  resource "jmespath" do
+    url "https://files.pythonhosted.org/packages/3c/56/3f325b1eef9791759784aa5046a8f6a1aff8f7c898a2e34506771d3b99d8/jmespath-0.10.0.tar.gz"
+    sha256 "b85d0567b8666149a93172712e68920734333c0ce7e89b78b3e987f71e5ed4f9"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
+    sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
+  end
+
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz"
     sha256 "607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e"
@@ -38,6 +64,16 @@ class Alibuild < Formula
   resource "requests" do
     url "https://files.pythonhosted.org/packages/e7/01/3569e0b535fb2e4a6c384bdbed00c55b9d78b5084e0fb7f4d0bf523d7670/requests-2.26.0.tar.gz"
     sha256 "b8aa58f8cf793ffd8782d3d8cb19e66ef36f7aba4353eec859e74678b01b07a7"
+  end
+
+  resource "s3transfer" do
+    url "https://files.pythonhosted.org/packages/88/ef/4d1b3f52ae20a7e72151fde5c9f254cd83f8a49047351f34006e517e1655/s3transfer-0.5.0.tar.gz"
+    sha256 "50ed823e1dc5868ad40c8dc92072f757aa0e653a192845c94a3b676f4a62da4c"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
 
   resource "urllib3" do
