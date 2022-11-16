67964b0c146b18ff012b00450d6b71cb39e292c1
Timo Wilken <timo.wilken@cern.ch>
2022-11-16 13:19:00 +0100

    Update llvm dependency

--- Formula/o2-full-deps.rb
+++ Formula/o2-full-deps.rb
@@ -39,7 +39,7 @@ class O2FullDeps < Formula
   depends_on "utf8proc"
   depends_on "libidn2"
   depends_on "gtk-doc"
-  depends_on "llvm@13"
+  depends_on "llvm@14"
   depends_on "clang-format"
 
   def install
