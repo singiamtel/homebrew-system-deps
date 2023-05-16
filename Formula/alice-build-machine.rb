fdf76a6a2764c0c358b758d8cbc022f8aa9ddb23
Timo Wilken <timo.wilken@cern.ch>
2023-05-16 13:09:04 +0200

    Remove obsolete MySQL package

--- Formula/alice-build-machine.rb
+++ Formula/alice-build-machine.rb
@@ -3,10 +3,9 @@ class AliceBuildMachine < Formula
   homepage "https://alisw.github.io"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "23.17-1"
+  version "23.20-1"
 
   depends_on "alisw/system-deps/o2-full-deps"
-  depends_on "mysql"
   depends_on "openjdk"
   depends_on "nomad"
   depends_on "consul"
