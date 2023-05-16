6392d48dca771d612f38456d9259b68bf70cc914
Timo Wilken <timo.wilken@cern.ch>
2023-05-16 11:08:14 +0000

    Install nomad and consul on build machines (#31)

--- Formula/alice-build-machine.rb
+++ Formula/alice-build-machine.rb
@@ -3,11 +3,13 @@ class AliceBuildMachine < Formula
   homepage "https://alisw.github.io"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "20.3-1"
+  version "23.17-1"
 
   depends_on "alisw/system-deps/o2-full-deps"
   depends_on "mysql"
   depends_on "openjdk"
+  depends_on "nomad"
+  depends_on "consul"
 
   def install
     system "touch", "#{prefix}/empty"
