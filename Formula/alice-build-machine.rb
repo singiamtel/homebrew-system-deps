9f00ed52e820e7a300aad83dfd177adf140ef5e4
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-05-26 17:05:53 +0200

    Bump version
    We didn't trigger the update properly when we changed the homebrew taps
    for nomad/consul

--- Formula/alice-build-machine.rb
+++ Formula/alice-build-machine.rb
@@ -3,7 +3,7 @@ class AliceBuildMachine < Formula
   homepage "https://alisw.github.io"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "23.48-1"
+  version "24.49-1"
 
   depends_on "alisw/system-deps/o2-full-deps"
   depends_on "openjdk"
